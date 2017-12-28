b21d1dbf7b7b77d607a20573acdcc2fd71e121ee
Aleksey Bogomolov <horse315@gmail.com>
2017-12-28 16:05:42 +0300

    fixed tests

--- src/test/scala/com/hypertino/services/apns/ApnsServiceTest.scala
+++ src/test/scala/com/hypertino/services/apns/ApnsServiceTest.scala
@@ -7,7 +7,7 @@ import com.hypertino.hyperbus.model.Accepted
 import com.hypertino.hyperbus.transport.api.ServiceRegistrator
 import com.hypertino.hyperbus.transport.registrators.DummyRegistrator
 import com.hypertino.service.config.ConfigLoader
-import com.hypertino.services.apns.api.{ApnsBadDeviceTokensFeedPost, ApnsNotification, ApnsPost, BadToken}
+import com.hypertino.services.apns.api.{ApnsBadDeviceTokensFeedPost, ApnsNotification, ApnsPost}
 import com.hypertino.services.apns.clients.ApnsClient
 import com.turo.pushy.apns.DeliveryPriority
 import com.turo.pushy.apns.util.SimpleApnsPushNotification
@@ -17,58 +17,59 @@ import monix.execution.Ack.Continue
 import monix.execution.Scheduler
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{BeforeAndAfterEach, FlatSpec}
+import scaldi.{DynamicModule, Injectable, Injector}
 
-import scala.concurrent.{Await, Future}
-import scala.concurrent.duration._
+import scala.concurrent.Future
 
 class ApnsServiceTest extends FlatSpec
   with BeforeAndAfterEach
   with TestServiceBase
-  with MockFactory {
-
-  implicit val scheduler: Scheduler = monix.execution.Scheduler.Implicits.global
-
-  bind [Config] to ConfigLoader()
-  bind [Scheduler] to scheduler
-  bind [Hyperbus] to injected[Hyperbus]
-  bind [ServiceRegistrator] to DummyRegistrator
+  with MockFactory
+  with Injectable {
 
   "ApnsService" should "send notification" in {
+    implicit val module: Injector = getModule
+
+    val hyperbus = inject[Hyperbus]
     val apnsClient = mock[ApnsClient]
 
     val mockResponse = TestPushNotificationResponse(new SimpleApnsPushNotification("token", "topic", "payload"), success = true, "", new Date())
 
     (apnsClient.sendNotification _).expects("token", "topic", "payload", None, DeliveryPriority.IMMEDIATE, None).returns(Task(mockResponse)).once
 
-    val apnsService = new ApnsService(apnsClient)(this)
+    val apnsService = new ApnsService(apnsClient)(module)
 
-    _hyperbus.ask(ApnsPost(new ApnsNotification("token", "topic", "payload")))
+    hyperbus.ask(ApnsPost(new ApnsNotification("token", "topic", "payload")))
       .runAsync
       .futureValue shouldBe a[Accepted[_]]
-
-    Await.ready(apnsService.stopService(false, 1.second), Duration.Inf)
   }
 
   it should "send feed-event on BadDeviceToken response" in {
+    implicit val module: Injector = getModule
+
+    val hyperbus = inject[Hyperbus]
     val apnsClient = mock[ApnsClient]
 
     val mockResponse = TestPushNotificationResponse(new SimpleApnsPushNotification("token", "topic", "payload"), success = false, "BadDeviceToken", new Date())
     (apnsClient.sendNotification _).expects("token", "topic", "payload", None, DeliveryPriority.IMMEDIATE, None).returns(Task(mockResponse)).once
 
-    val apnsService = new ApnsService(apnsClient)(this)
-    val serviceMock = new ServiceMock(_hyperbus)
+    val apnsService = new ApnsService(apnsClient)
+    val serviceMock = new ServiceMock(hyperbus)
 
     val mockedMethods = mock[MockedMethods]
     (mockedMethods.onApnsBadDeviceTokensFeedPost _).expects(where { rq: ApnsBadDeviceTokensFeedPost => rq.body.deviceToken == "token" && rq.body.bundleId == "topic" }).returns(Future { Continue }).once
     serviceMock.mockHandler = Some(mockedMethods)
 
-    _hyperbus.ask(ApnsPost(new ApnsNotification("token", "topic", "payload")))
+    hyperbus.ask(ApnsPost(new ApnsNotification("token", "topic", "payload")))
       .runAsync
       .futureValue shouldBe a[Accepted[_]]
+  }
 
-    // waiting at least 1 second for feed put
-    Thread.sleep(1000)
-    Await.ready(Future.sequence(Seq(apnsService.stopService(false, 1.second),
-      serviceMock.stopService(false, 1.second))), Duration.Inf)
+  def getModule: Injector = DynamicModule { implicit module =>
+    val configLoader = ConfigLoader()
+    module.bind[Config] to configLoader
+    module.bind[Scheduler] to _scheduler
+    module.bind[Hyperbus] to new Hyperbus(configLoader)
+    module.bind[ServiceRegistrator] to DummyRegistrator
   }
 }
--- src/test/scala/com/hypertino/services/apns/TestServiceBase.scala
+++ src/test/scala/com/hypertino/services/apns/TestServiceBase.scala
@@ -17,38 +17,15 @@ import scaldi.Module
 import scala.collection.mutable
 import scala.concurrent.duration._
 
-trait TestServiceBase extends TestSuite with Module with Subscribable
-  with BeforeAndAfterAll
+trait TestServiceBase extends TestSuite
+  with Subscribable
   with ScalaFutures
   with Eventually
   with Matchers {
 
   implicit val patience = PatienceConfig(scaled(Span(20, Seconds)))
 
-  protected implicit val _scheduler = monix.execution.Scheduler.Implicits.global
-  bind [Config] to ConfigLoader()
-  bind [Scheduler] to _scheduler
-  bind [Hyperbus] to injected[Hyperbus]
-  bind [ServiceRegistrator] to DummyRegistrator
-
-  protected val _hyperbus = inject[Hyperbus]
-
-  protected val _handlers = mutable.ArrayBuffer[Cancelable]()
-  protected val _services = mutable.ArrayBuffer[Service]()
   protected implicit val mcx = MessagingContext.empty
-  implicit val so = com.hypertino.hyperbus.serialization.SerializationOptions.default
-  import so._
 
-  def start(handlers: Seq[Cancelable] = Seq.empty, services: Seq[Service] = Seq.empty): Unit = {
-    _handlers ++= handlers
-    _services ++= services
-
-    Thread.sleep(1500)
-  }
-
-  override def afterAll() {
-    _services.foreach(_.stopService(false, 10.seconds).futureValue)
-    _handlers.foreach(_.cancel())
-    _hyperbus.shutdown(10.seconds).runAsync.futureValue
-  }
+  protected implicit val _scheduler = monix.execution.Scheduler.Implicits.global
 }
